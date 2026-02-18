8815a5384d9b229181aee0ae154b0403464e5504
Amaan Mansoori <mansoori280904@gmail.com>
2026-02-18 13:40:29 +0530

    Added Google Search verification

---
+++ src/app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+    return [
+        {
+            url: "https://spareindiacorporation.vercel.app",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://spareindiacorporation.vercel.app/catalog",
+            lastModified: new Date(),
+        },
+    ];
+}
